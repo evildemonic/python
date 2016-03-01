4e17e9251070027df83695c6da87674ab212ee71
Jenny <jenabink@gmail.com>
2016-03-01 05:43:54 -0500

    Update unit1_speedoflight.py

--- cs101/unit1_speedoflight.py
+++ cs101/unit1_speedoflight.py
@@ -4,4 +4,9 @@
 # speed_of_light = 299792458   meters per second
 # centimeters = 100            one meter is 100 centimeters
 # nanosecond = 1.0/1000000000  one billionth of a second
-print 299792458 * 100 * 1.0/1000000000
+
+speed_of_light = 299792458
+centimeters = 100
+nanosecond = 1.0/1000000000
+
+print speed_of_light * centimeters * nanosecond
