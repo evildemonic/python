5f3e483d33d19b8f9f0a3946c2b63c8921ee0485
Jenny <jenabink@gmail.com>
2016-03-01 11:42:52 -0500

    Update string1.py

--- google/string1.py
+++ google/string1.py
@@ -24,10 +24,7 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  if count < 10:
-    return 'Number of donuts: ' + str(count)
-  else:
-    return 'Number of donuts: many'
+  # +++your code here+++
   return
 
 
